2e8dce10f128530c81c6516ae40baafc542c0504
markrissmiller <mprissmi@gmail.com>
2021-05-20 18:58:23 -0400

    make it better

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,7 +6,7 @@ const router  = express.Router()
 
 export const getPosts = async (req, res) => {
     try {
-        const postMessages = await PostMessage.find();
+        const postMessages = await PostMessage.find({});
 
         res.status(200).json(postMessages);
 
